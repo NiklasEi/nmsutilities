acf84a923f2b4272e2fed744a29a74fe7ca2698e
Niklas Eicker <git@nikl.me>
2020-03-15 01:32:57 +0100

    add script to install all Bukkit dependencies

---
+++ install_all_versions.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+versions=( "1.8" "1.8.3" "1.8.8" "1.9.2" "1.9.4" "1.10.2" "1.11.2" "1.12.2" "1.13" "1.13.1" "1.14.1" "1.15" )
+
+for version in ${versions[@]}
+do
+  java -jar BuildTools.jar --rev ${version}
+done
